5379d35873b25267cf523315bee0909a17bbf66e
MasuDeokar <mayureshdeokar@gmail.com>
2024-01-25 10:28:24 +0530

    25-01

--- src/test/java/masudeokar/TestComponents/BaseTest.java
+++ src/test/java/masudeokar/TestComponents/BaseTest.java
@@ -10,10 +10,12 @@ import java.util.List;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
@@ -37,9 +39,17 @@ public class BaseTest {
 		String browserName =System.getProperty("browser")!=null?System.getProperty("browser"):prop.getProperty("browser");
 		//String browserName = prop.getProperty("browser");
 
-		if (browserName.equalsIgnoreCase("chrome")) {
+		if (browserName.contains("chrome")) {
+							
 			System.setProperty("webdriver.chrome.driver","./ServerOne/chromedriver.exe");
 			driver = new ChromeDriver();
+			ChromeOptions options = new ChromeOptions();
+			if(browserName.contains("headless"))
+			{
+			options.addArguments("headless");
+			}
+			driver = new ChromeDriver(options);
+			driver.manage().window().setSize(new Dimension(1440,900)); // full screen
 
 		} else if (browserName.equalsIgnoreCase("firefox")) {
 			System.setProperty("webdriver.geko.driver","./ServerOne/geckodriver.exe");
